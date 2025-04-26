b2670e10f1a1e28f68c3f661f77ea4739bcfbed0
Seyia07 <seyiasankok@gmail.com>
2025-04-26 15:29:07 +0300

    Update sum_of_list.py

--- sum_of_list.py
+++ sum_of_list.py
@@ -1,9 +1,1 @@
-def list_sum(numbers):
-    sum = 0 #Included a variable to which all numbers would be added to there
-    for number in numbers:
-        sum += number
-    return sum
 
-marks = [10,20,30,40]
-sum = list_sum(marks) #Returns 100
-print(sum)
