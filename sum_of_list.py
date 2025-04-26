c4c4dfed76adc3f513bdf07c865795e53657de03
godfreyM04 <gomark026@gmail.com>
2025-04-26 14:42:09 +0300

    commit

--- sum_of_list.py
+++ sum_of_list.py
@@ -2,7 +2,7 @@ def list_sum(numbers):
     sum = 0 #Included a variable to which all numbers would be added to there
     for number in numbers:
         sum += number
-    return sum 
+    return sum
 
 marks = [10,20,30,40]
 sum = list_sum(marks) #Returns 100
